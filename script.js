44e7bbc32d59d91d1d753d56465e32e47d604df2
Pedro Cunha <72658683+CunhaPedro25@users.noreply.github.com>
2021-02-27 02:40:33 +0000

    Added Hidden Buttons & Random images Smooth
    Now the random image background is smoother and its possible to toggle the buttons to appear or not by clicking on the background image

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ var darktheme = false;
 var selectedTimer = 1;
 var defaultTime = tempo;
 var open = false;
+var hidden = false;
 let btns = document.querySelectorAll("[data-button]");
 let timerBtns = document.querySelectorAll("[data-timer]");
-var i = 1;
+var i = 2;
 
 function next(){
     tempo = defaultTime;
-    i++
+    if(i == 1000)
+        i = 1;
 
-    document.body.style.backgroundImage = "url('https://picsum.photos/1920/1080?random=" + i + "')";
+    if(i == 2)
+        document.getElementById("background-visible").style.backgroundImage = "url('https://picsum.photos/1920/1080?random=2')"
+    else
+        document.getElementById("background-visible").style.backgroundImage = document.getElementById("background-notvisible").style.backgroundImage; 
+
+    i++
+    document.getElementById("background-notvisible").style.backgroundImage = "url('https://picsum.photos/1920/1080?random=" + i + "')";
 }
 
 if(tempo != -1){
@@ -24,7 +32,7 @@ if(tempo != -1){
     },1000);
 }
 
-function info(){
+function menu(){
     open = !open
     btns[0].style.backgroundColor = "lightgrey";
 
@@ -39,12 +47,31 @@ function info(){
     }
 }
 
+function hideControls(){
+    hidden = !hidden;
+
+    if(hidden){
+        document.getElementById("eye").innerHTML = "<path d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z'/><path d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z'/><path d='M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884l-12-12 .708-.708 12 12-.708.708z'/>"
+        document.getElementById("button-line").style.opacity = 0;
+        document.getElementById("button-line").style.visibility = "hidden";
+        document.getElementById("dropdown-content").style.opacity = 0;
+        document.getElementById("dropdown-content").style.visibility = "hidden";
+    }else{
+        document.getElementById("eye").innerHTML = "<path d='M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z'/><path d='M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z'/>"
+        document.getElementById("button-line").style.opacity = 1;
+        document.getElementById("button-line").style.visibility = "visible";
+        document.getElementById("dropdown-content").style.opacity = 1;
+        document.getElementById("dropdown-content").style.visibility = "visible";
+    }
+}
+
+
 function changeTheme(){
     darktheme = !darktheme;
 
     if(darktheme == true){
-        document.getElementById("dropdown-content").style.backgroundColor = "#161b22";
-        document.getElementById("dropdown-content").style.color = "white";
+        document.body.style.backgroundColor = "#161b22";
+        document.body.style.color = "white";
         btns.forEach(element => element.style.backgroundColor = "#161b22");
         btns.forEach(element => element.style.color = "white");
 
@@ -53,8 +80,8 @@ function changeTheme(){
         document.getElementById("TimerSelector-content").style.color = "white";
         timerBtns.forEach(element => element.style.backgroundColor = "#161b22");
     }else{
-        document.getElementById("dropdown-content").style.backgroundColor = "white";
-        document.getElementById("dropdown-content").style.color = "black";
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
         btns.forEach(element => element.style.backgroundColor = "white");
         btns.forEach(element => element.style.color = "black");
 
@@ -101,12 +128,5 @@ window.onclick = function(event) {
     if (event.target == document.getElementById("TimerSelector")) {
         document.getElementById("TimerSelector").style.display = "none";
     }
+    
 }
-
-/**
- * Testes de leitura
- */
-//let btns = document.querySelectorAll("[data-button]")
-//btns.forEach(element => element.style.backgroundColor = "black")
-//Inline
-//document.querySelectorAll("[data-button]").forEach(element => element.style.backgroundColor = "black")
